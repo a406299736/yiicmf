cc5973abc2f39a683833ef3666fe0881ee0f8e32
Jin <1272932541@qq.com>
2017-03-01 17:37:32 +0800

    linux开启定时任务,本地为开启

--- console/schedule.php
+++ console/schedule.php
@@ -15,3 +15,14 @@
 // $schedule->command('migrate')->cron('* * * * *');
 
 // $schedule->exec('composer self-update')->daily();
+
+/*
+linux
+
+crontab -e 添加如下一条定时任务
+
+* * * * * php /path/to/yii schedule/run 1>> /dev/null 2>&1
+
+console/schedule.php 里添加一条需要定时执行的Yii命令
+
+$schedule->exec('migrate/dump')->daily(); // 每天执行一次数据库导出迁移*/
