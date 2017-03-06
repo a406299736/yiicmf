5dfe57c0f6bc7a8c33753c2195616823f67c764b
Jin <1272932541@qq.com>
2017-03-06 10:37:38 +0800

    common/config

---
+++ common/config/main.php
@@ -0,0 +1,96 @@
+<?php
+
+return [
+    'vendorPath' => dirname(dirname(__DIR__)).'/vendor',
+    'runtimePath' => '@app/runtime',
+    'timezone' => 'PRC',
+    'language' => 'zh-CN',
+    'bootstrap' => [
+        'log',
+        'common\\components\\LoadModule',
+        'common\\components\\LoadPlugins',
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+            'cachePath' => '@root/cache',
+            'dirMode' => 0777 // 防止console生成的目录导致web账户没写权限
+        ],
+        'formatter' => [
+            'dateFormat' => 'yyyy-MM-dd',
+            'datetimeFormat' => 'yyyy-MM-dd HH:mm:ss',
+            'timeFormat' => 'HH:mm:ss',
+            'decimalSeparator' => '.',
+            'thousandSeparator' => ' ',
+            'currencyCode' => 'CNY',
+        ],
+        'assetManager' => [
+            'bundles' => [
+                'yii\web\YiiAsset' => [
+                    'sourcePath' => '@common/static',
+                    'depends' => [
+                        'common\assets\ModalAsset'
+                    ]
+                ],
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                '*'=> [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath'=>'@common/messages',
+                    'fileMap'=>[
+                        'common'=>'common.php',
+                        'backend'=>'backend.php',
+                        'frontend'=>'frontend.php',
+                    ],
+                    'on missingTranslation' => ['\backend\modules\i18n\Module', 'missingTranslation']
+                ],
+                /*'*'=> [
+                    'class' => 'yii\i18n\DbMessageSource',
+                    'sourceMessageTable'=>'{{%i18n_source_message}}',
+                    'messageTable'=>'{{%i18n_message}}',
+                    'enableCaching' => YII_ENV_DEV,
+                    'cachingDuration' => 3600,
+                    'on missingTranslation' => ['\backend\modules\i18n\Module', 'missingTranslation']
+                ],*/
+            ],
+        ],
+        'storage' => [
+            'class' => 'common\\components\\Storage',
+            'fs' => [
+                //'class' => 'creocoder\flysystem\LocalFilesystem',
+                //'path' => '@storagePath/upload',
+                'class' => 'common\\components\\flysystem\\QiniuFilesystem',
+                'access' => '',
+                'secret' => '',
+                'bucket' => '',
+            ],
+            //'baseUrl' => '@storageUrl/upload'
+            'baseDir' => '@webroot',
+            'baseUrl' => '',
+        ],
+        'log' => [
+            'targets' => [
+                'db'=>[
+                    'class' => 'yii\log\DbTarget',
+                    'levels' => ['warning', 'error'],
+                    'except'=>['yii\web\HttpException:*', 'yii\i18n\I18N\*'],
+                    'prefix'=>function () {
+                        $url = !Yii::$app->request->isConsoleRequest ? Yii::$app->request->getUrl() : null;
+                        return sprintf('[%s][%s]', Yii::$app->id, $url);
+                    },
+                    'logVars'=>[],
+                    'logTable'=>'{{%system_log}}'
+                ],
+            ]
+        ],
+        'notify' => 'common\components\notify\Handler',
+        'moduleManager' => [
+            'class' => 'common\\components\\ModuleManager'
+        ],
+        'pluginManager' => [
+            'class' => 'common\components\PluginManager',
+        ],
+    ],
+];
